b11bf8d9d04bf6afc2168a8c0d5a7e4717442b99
ellzmarie <elliematutisdigal@gmail.com>
2023-02-11 18:15:31 -0800

    updates migrations

--- people_api/models.py
+++ people_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Person(models.Model):
   name = models.CharField(max_length=100)
-  image = models.CharField(max_length=100)
+  image = models.CharField(max_length=300)
   title = models.CharField(max_length=100)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
